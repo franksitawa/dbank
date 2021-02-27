71a97ada1fdd1b0701ab7e4afb0a647eff4e9893
Frank Sitawa <franksitawa@gmail.com>
2021-02-27 16:02:33 +0300

    Contract commit

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -4,18 +4,28 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  //add minter variable
+  address public minter;
 
   //add minter changed event
+  event MinterChanged(address indexed from,address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
+  constructor() public payable ERC20("Elikia Bank", "EB") {
+    minter=msg.sender;
   }
 
   //Add pass minter role function
 
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
+    require(msg.sender==minter,"Error, sender has no minter role");
 		_mint(account, amount);
 	}
+
+  function passMinterRole(address dBank) public returns (bool){
+    require(msg.sender==minter,"Error, sender has no minter role");
+
+    minter=dBank;
+    emit MinterChanged(msg.sender,dBank);
+    return true;
+  }
 }
--- src/contracts/dBank.sol
+++ src/contracts/dBank.sol
@@ -6,14 +6,21 @@ import "./Token.sol";
 contract dBank {
 
   //assign Token contract to variable
+  Token private token;
 
   //add mappings
+  mapping(address=>uint) public etherBalanceOf;
+  mapping(address=>uint) public depositStart;
+  mapping(address=>uint) public isDeposited;
 
   //add events
 
+  event Deposit(address indexed user,uint etherAmount,uint timeStart);
+  event Withdraw(address indexed user,uint etherAmount,uint depositTime,uint interest);
   //pass as constructor argument deployed Token contract
-  constructor() public {
+  constructor(Toke _token) public {
     //assign token deployed contract to variable
+    token=_token;
   }
 
   function deposit() payable public {
@@ -22,26 +29,42 @@ contract dBank {
 
     //increase msg.sender ether deposit balance
     //start msg.sender hodling time
+    etherBalanceOf[msg.sender]=etherBalanceOf[msg.sender]+msg.value;
+    depositStart[msg.sender]=depositStart[msg.sender]+block.timestamp;
+    isDeposited[msg.sender]=true;
 
     //set msg.sender deposit status to true
     //emit Deposit event
+    emit Deposit(msg.sender, msg.value,block.timestamp);
   }
 
   function withdraw() public {
     //check if msg.sender deposit status is true
     //assign msg.sender ether deposit balance to variable for event
+    require(isDeposited[msg.sender]==true,"Error, no previous deposit");
+    uint userBalance=etherBalanceOf[msg.sender];
 
     //check user's hodl time
+    uint depositTime= block.timestamp+depositStart[msg.sender];
 
     //calc interest per second
+    uint interestPerSecond=31668017*(etherBalanceOf[msg.sender]/1e16);
+    uint interest=interestPerSecond*depositTime;
     //calc accrued interest
 
     //send eth to user
+    msg.sender.transfer(etherBalanceOf[msg.sender]);
+    token.mint(msg.sender,interest);
     //send interest in tokens to user
 
-    //reset depositer data
 
+
+    //reset depositer data
+    etherBalanceOf[msg.sender]=0;
+    depositStart[msg.sender]=0;
+    isDeposited[msg.sender]=false;
     //emit event
+    emit Withdraw(msg.sender,userBalance,depositTime,interest);
   }
 
   function borrow() payable public {
